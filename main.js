dbe648341c795a5ecfa8f9d766931f559af0f9ff
Сергей Казарезов <serjino777@yandex.ru>
2021-07-19 12:14:31 +0300

    some media query

--- main.js
+++ main.js
@@ -1,27 +1,73 @@
 document.addEventListener('DOMContentLoaded', function () {
 
+  // Accordion init
+
   $("#accordion").accordion({
     heightStyle: "content",
     collapsible: true,
     active: false,
   });
 
+  // =============================================
+
+  // Swiper init
+
+  const swiper = new Swiper('.swiper-container', {
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
+
+  // =============================================
+
+  // Cancel focus effects, when you click on the element
+
+  function blurByMouseUp(CSSClass) {
+    let elements = document.getElementsByClassName(CSSClass);
+    for (let el of elements) {
+      el.addEventListener('mouseup', function () {
+        this.blur();
+      });
+    }
+  }
+
+  blurByMouseUp('logo__link');
+  blurByMouseUp('btn');
+
+  // =============================================
+
+  // Carousel init
+
   let btns = document.querySelectorAll('.carousel__controls-step__button');
   let contentItems = document.querySelectorAll('.carousel__item');
-  let accordionHeaders = document.querySelectorAll('.ui-accordion-header');
 
-  btns.forEach(function callback(btn, btnIndex) {
-    btn.addEventListener('click', function (event) {
-      btns.forEach(function callback(btn) {
+  btns.forEach(function callback (btn, btnIndex) {
+    btn.addEventListener('click', function(event){
+      btns.forEach(function callback (btn){
         btn.classList.remove('carousel__controls-step__button_active');
       })
       event.target.classList.add('carousel__controls-step__button_active');
-      contentItems.forEach(function callback(contentItem) {
+      contentItems.forEach(function callback (contentItem) {
         contentItem.classList.remove('carousel__item_active');
       });
       contentItems[btnIndex].classList.add('carousel__item_active');
     });
   });
+
+  // =============================================
+
+  // Adding hover styles on accordion header
+
+  let accordionHeaders = document.querySelectorAll('.ui-accordion-header');
 
   accordionHeaders.forEach(function callback(accordionHeader, index) {
 
@@ -52,6 +98,10 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   });
 
+  // =============================================
+
+  // Mobile navigation menu show/hide effects
+
   document.body.addEventListener('click', function (event) {
     console.log('s');
     if (event.target == document.querySelector('.burger-menu__button') || event.target == document.querySelector('.dark-scene_active')) {
@@ -66,10 +116,21 @@ document.addEventListener('DOMContentLoaded', function () {
     }
   });
 
+  // =============================================
+
+  // Checking terms policy aggrement acception
+
   document.querySelector('.request-form').addEventListener('keydown', function (e) {
     if (e.keyCode === 13 && e.target == document.querySelector('.request-form__input_agr-accept')) {
       e.target.checked = !e.target.checked;
       e.preventDefault();
     }
   })
+
+  // =============================================
+
+  window.addEventListener('load', function () {
+    document.body.style.opacity = '1';
+  });
+
 });
